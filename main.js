359107be30d29e017cf714c710764c9fd0011790
katarinarosiak <64321232+katarinarosiak@users.noreply.github.com>
2020-06-01 12:13:51 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -16,26 +16,134 @@ function removeElement(toRemove, fromWhere) {
 	let tempArr = arrAfterRemoved.splice(arrAfterRemoved.indexOf(toRemove), 1);
 	return arrAfterRemoved;
 }
-//Function that will take an elemenet and print it on the chosen element of a page if empty
-function buttonClicked(what, where){
+//Function that checks if the value of an object is empty
+function checkIfEmpty(obj){
+	if(obj.value === " "){
+		return true
+	}else{
+		return false
+		}
+	};
 
+function changePlayer(player){
+	if(player.name === "Player 1"){
+		currentPlayer = Player2;
+		Player2.yourTurn = true;
+		Player1.youtTurn = false; 
+	}else{
+		currentPlayer = Player1;
+		Player1.yourTurn = true;
+		Player2.yourTurn = false; 
+	}
 }
 
+ function checkIfWin(){
+ 	if(a1.value + a2.value + a3.value === "XXX" || a1.value + a2.value + a3.value === "OOO"){
+ 		return true;
+ 	}else if(b1.value + b2.value + b3.value === "XXX" || b1.value + b2.value + b3.value === "OOO"){
+ 		return true;
+ 	}else if(c1.value + c2.value + c3.value === "XXX" || c1.value + c2.value + c3.value === "OOO"){
+ 		return true;
+ 	}else if(a1.value + b1.value + c1.value === "XXX" || a1.value + b1.value + c1.value === "OOO"){
+ 		return true;
+ 	}else if(a2.value + b2.value + c2.value === "XXX" || a2.value + b2.value + c2.value === "OOO"){
+ 		return true;
+ 	}else if(a3.value + b3.value + c3.value === "XXX" || a3.value + b3.value + c3.value === "OOO"){
+ 		return true;
+ 	}else if(b1.value + b2.value + b3.value === "XXX" || b1.value + b2.value + b3.value === "OOO"){
+ 		return true;
+ 	}else if(a1.value + b2.value + c3.value === "XXX" || a1.value + b2.value + c3.value === "OOO"){
+ 		return true;
+ 	}else if(a3.value + b2.value + c1.value === "XXX" || a3.value + b2.value + c1.value === "OOO"){
+ 		return true;
+ 	}else{
+ 		return false;
+ 	}
+ }
+
+
+ function addPoints(player, currentPoint){
+ 	console.log(player);
+ 	console.log(currentPoint);
+ 	currentPoint = currentPoint +1;
+ 	console.log(currentPoint);
+ 	return currentPoint;
+ }
+ 					
+ function checkIfDraw(){
+ 	let allCells;
+ 	allCells = a1.value + a2.value + a3.value + b1.value + b2.value + b3.value + c1.value + c2.value + c3.value;
+ 	return allCells.includes(" ");
+ }
+
+ function playAgainDraw(){
+ 	round++;
+	changeOnScreen(round ,"roundId");
+	clearTheBoard();
+ }
+
+ function clearTheBoard() {
+ 	a1.value = " ";
+ 	a1.empty = true;
+ 	changeOnScreen(" ", "a1");
+ 	a2.value = " ";
+ 	a2.empty = true;
+ 	changeOnScreen(" ", "a2");
+ 	a3.value = " ";
+ 	a3.empty = true;
+ 	changeOnScreen(" ", "a3");
+ 	b1.value = " ";
+ 	b1.empty = true;
+ 	changeOnScreen(" ", "b1");
+ 	b2.value = " ";
+ 	b2.empty = true;
+ 	changeOnScreen(" ", "b2");
+ 	b3.value = " ";
+ 	b3.empty = true;
+ 	changeOnScreen(" ", "b3");
+ 	c1.value = " ";
+ 	c1.empty = true;
+ 	changeOnScreen(" ", "c1");
+ 	c2.value = " ";
+ 	c2.empty = true;
+ 	changeOnScreen(" ", "c2");
+ 	c3.value = " ";
+ 	c3.empty = true;
+ 	changeOnScreen(" ", "c3");
+ }
+
+ function resetGame(){
+ 	clearTheBoard();
+ 	//clear rounds
+ 	changeOnScreen(1, "roundId");
+ 	round = 1;
+ 	//clear score
+ 	Player1.score = 0;
+ 	Player2.score = 0;
+ 	changeOnScreen(0, "player1score");
+ 	changeOnScreen(0, "player2score");
+
+ }
+
+//--//--//--//--//--//--//--//--//--//--//--//--///--//--//--//--//--//--//--//--//--//--//--//--/
 //create User objects
 class User {
-	constructor(name, sign, score, yourTurn) {
+	constructor(name, sign, score, yourTurn, elementId) {
 		this.name = name;
 		this.sign = sign;
 		this.score = score;
 		this.yourTurn = yourTurn;
+		this.elementId = elementId;
 	}
 }
-	let Player1 = new User("Player 1", " ", 0, false);
-	let Player2 = new User("Player 2", " ", 0, false);
+	let Player1 = new User("Player 1", " ", 0, true, "player1score");
+	let Player2 = new User("Player 2", " ", 0, false, "player2score");
 
 
-
+let currentPlayer = Player1;
 let gameSigns = ["X", "O"];
+let round = 1; 
+
 //function that reactivates by clicking "start" button.  
 //Choose which sign will player 1 have and give remaining to Player 2
 function start(elements, chosenElPlace, remainElPlace) {
@@ -44,14 +152,16 @@ function start(elements, chosenElPlace, remainElPlace) {
 	let signs = elements;
 	const chosenSign = randomize(elements);
 	changeOnScreen(chosenSign, chosenElPlace);
+//Assign the sign to Player1 object 
 	Player1.sign = chosenSign;
-//print gameSign that is left for player 2
-	let otherSign = 
-	changeOnScreen(removeElement(chosenSign, signs), remainElPlace);
-	Player2.sign = otherSign
+//print gameSign that is left for player 2.
+	let otherSign = removeElement(chosenSign, signs);
+	changeOnScreen(otherSign, remainElPlace);
+//Assign the sign to Player2 object 	
+	Player2.sign = otherSign[0];
+	const button = document.getElementById('startbutton');
+	button.disabled = true;
 }
-
-
 
 //make each cell to an object  
 class Cell {
@@ -61,14 +171,8 @@ class Cell {
         this.color = color;
         this.win = win;
     }
-    writeSign() {
-        console.log("this method will change a sign to x or o whenever button clicked");
-    }
-    changeColor() {
-    	console.log("this method will change the color of cell whenever win");
-    }
+   
 }
-
 		let a1 = new Cell(" ", true, "grey", false);
 		let a2 = new Cell(" ", true, "grey", false);
 		let a3 = new Cell(" ", true, "grey", false);
@@ -79,12 +183,52 @@ class Cell {
 		let c2 = new Cell(" ", true, "grey", false);
 		let c3 = new Cell(" ", true, "grey", false);
 
+//Creating an array with objects values
 
+//Racting on a clicked button and performing a move 
+function buttonClicked(player, where, obj){
+	if(obj.empty === true){
+	//Print on a screen chosen sign of the current player
+	 	changeOnScreen(player.sign, where);
+	//Change turn
+	 	changePlayer(player);
+	//change empty to false
+	 	obj.empty = false;
+	//add the sign as a value to the object connected to the cell
+		obj.value = player.sign;
+	//check if there is a win 
+		if (checkIfWin() === true){
+			//if there is a win print congratulation and add scores
+			let respond = confirm("Congratulation you have won! Do you want to play again?");
+				if (respond == true) {
+  				
+// !!!!!!!!!!!!!!!!! let current score is only one player?? what the fack??
+ 
+  				player.score = player.score +1;
+  				let currentScore = player.score;
+  				console.log(currentScore);
+  				//let currentScore = addPoints(player, player.score);
+				changeOnScreen(currentScore, player.elementId);
+				round++;
+				clearTheBoard();
+				changeOnScreen(round ,"roundId");
+				} else {
+  				resetGame();
+				}
 
+	//check if there is a draw
+		}else if(checkIfDraw() === false){
+			let draw = confirm("It's a draw. Do you want to play again?");
+				if (draw == true) {
+					playAgainDraw();
+				}else {
+					resetGame();
+				}
+		}	
 
-
-
-	
+	}
+}
+//activate reste game button
 
 
 
